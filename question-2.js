6e90d366bf0d9a2eef775b5bfa949e8113dc321c
Isaiah <40073397+Isaiahsylnol@users.noreply.github.com>
2020-10-08 20:52:06 -0400

    Update question-2.js

--- question-2.js
+++ question-2.js
@@ -13,3 +13,37 @@
         }
 
         resolvedPromise(500);
+
+     const resolvedPromise = (ms) => {
+            setTimeout(() => {
+                var promise = new Promise(function(resolve, reject) {
+                    if(ms >= 500) {
+                        resolve("Message : delayed promise");
+                    }
+                    else{
+                        reject(Error("It broke"));
+                    }
+                });
+                console.log(promise);
+            }, ms)
+        }
+
+        resolvedPromise(500);
+
+
+  const rejectedPromise  = (ms) => {
+            try {
+                setTimeout(() => {
+                if(ms <= 500){
+                      var promise = new Promise(function(resolve, reject) {
+                        resolve("Message : delayed exception!");     
+                });
+                }
+                console.log(promise);
+            }, ms)
+                throw new Error("Error: rejected time");
+            } catch(e){
+                console.error(e);
+            }
+        }
+        rejectedPromise(500);
